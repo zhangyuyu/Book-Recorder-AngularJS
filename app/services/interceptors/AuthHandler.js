dbf630bae80c6f932539fb824315df71fa0cc9de
yuzhang <1580074674@qq.com>
2016-08-07 10:37:53 +0800

    [Yu] 添加AuthHandle的interceptor处理用户名密码错误.

--- app/services/interceptors/AuthHandler.js
+++ app/services/interceptors/AuthHandler.js
@@ -1,27 +1,15 @@
 'use strict';
-angular.module('app').factory('AuthHandler', function ($q) {
-  var AuthHandler = {};
-  AuthHandler.request = function (config) {
-    if (config.data && config.data.$skipAuthHandler) {
-      config.$skipAuthHandler = true;
-      delete config.data.$skipAuthHandler;
-    }
-    if (config.params && config.params.$skipAuthHandler) {
-      config.$skipAuthHandler = true;
-      delete config.params.$skipAuthHandler;
-    }
-    return $q.when(config);
-  };
-  AuthHandler.responseError = function (rejection) {
-    if (rejection.status === 401 && rejection.config && !rejection.config.$skipAuthHandler && !rejection.config.url.match(/.*\/captcha.jpg$/)) {
-      // 如果已经弹出了对话框，则不再弹出，而是把当前的request追加到列表
-      var deferred = $q.defer();
-      // TODO: 实现登录重试逻辑
-      console.error('请自行实现登录重试逻辑，参见http://witoldsz.github.io/angular-http-auth/');
-      return deferred.promise;
-    } else {
-      return $q.reject(rejection);
+
+angular.module('app').factory('AuthHandler', function AuthHandlerFactory($q, $injector) {
+  return {
+    responseError: function (rejection) {
+      // 如果服务器返回了401 unauthorized，那么就表示需要登录
+      if (rejection.status === 401) {
+        alert('用户名或密码错误!');
+      } else {
+        // 其它错误不用管，留给其它interceptor去处理
+        return $q.reject(rejection);
+      }
     }
   };
-  return AuthHandler;
 });
